7165273742f6f94c7b8908cf440976c0144bfda4
Francesco Romani <fromani@redhat.com>
2021-03-31 19:02:33 +0200

    bump copyright year
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/numalign/main.go
+++ cmd/numalign/main.go
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Copyright 2020 Red Hat, Inc.
+ * Copyright 2020-2021 Red Hat, Inc.
  */
 
 package main
